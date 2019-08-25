8cbc79bc8d02a01c9d9b391d649f0527ecaa0001
Gil Tayar <gil@tayar.org>
2019-08-25 07:20:53 +0300

    hide e2e test

--- test/e2e/adding-user.e2e.test copy.js
+++ test/e2e/adding-user.e2e.test copy.js
@@ -2,14 +2,14 @@
 const path = require('path')
 const {describe, it, before, after, beforeEach} = require('mocha')
 const {expect} = require('chai')
-// const {v4: uuid} = require('uuid')
+const {v4: uuid} = require('uuid')
 const {
-  // fetchAsJsonWithJsonBody,
-  // fetchAsJson,
+  fetchAsJsonWithJsonBody,
+  fetchAsJson,
 } = require('@applitools/http-commons')
 const {dockerComposeTool} = require('@applitools/docker-compose-mocha')
 const {
-  // getAddressForService,
+  getAddressForService,
 } = require('@applitools/docker-compose-testkit')
 const {prepareDatabase, resetDatabase} = require('../commons/setup')
 
@@ -21,6 +21,22 @@ describe('microservices-are-made-for-testing (e2e)', function() {
   beforeEach(() => resetDatabase(envName, composePath))
 
   it('should return users after they are added', async () => {
-    expect(true).to.be.true
+    const appAddress = await getAddressForService(
+      envName,
+      composePath,
+      'app',
+      80,
+    )
+
+    const tenant = {id: uuid(), firstName: 'Gil', lastName: 'Tayar'}
+
+    await fetchAsJsonWithJsonBody(
+      `http://${appAddress}/api/tenants/${tenant.id}`,
+      tenant,
+    )
+
+    expect(
+      await fetchAsJson(`http://${appAddress}/api/tenants`),
+    ).to.eql([tenant])
   })
 })
