2c8d3c80870316f1cfafa2820822530ce8c25317
Gil Tayar <gil@tayar.org>
2020-05-31 11:43:30 +0300

    renamed a variable

--- test/it/setup-app.js
+++ test/it/setup-app.js
@@ -1,14 +1,9 @@
-import dockerComponse from '@applitools/docker-compose-testkit'
+import dct from '@applitools/docker-compose-testkit'
 
 import createApp from '../../src/microservices-are-made-for-testing.js'
 
 export async function setupApp(envName, composePath) {
-  const postgresAddress = await dockerComponse.getAddressForService(
-    envName,
-    composePath,
-    'postgres',
-    5432,
-  )
+  const postgresAddress = await dct.getAddressForService(envName, composePath, 'postgres', 5432)
   const connectionString = `postgresql://user:password@${postgresAddress}/postgres`
 
   const appInstance = await createApp({
